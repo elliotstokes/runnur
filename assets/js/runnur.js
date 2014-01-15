f2fe90dc72291ca2522121c65c592d7b630926fc
Elliot Stokes <elliot.stokes@gmail.com>
2014-01-15 22:56:20 +0000

    add geocoded and style

--- assets/js/runnur.js
+++ assets/js/runnur.js
@@ -1,8 +1,15 @@
   require([
-      "esri/arcgis/utils",
-      "dojo/domReady!"
-      ], function(arcgisUtils){
-      arcgisUtils.createMap("92309d85b34342de8514caefa3df56a5", "map").then(function (response) {
-      	//todo: add stuff 
-      });
+  	"esri/arcgis/utils",
+  	"esri/dijit/Geocoder",
+  	"dojo/domReady!"
+  ], function(arcgisUtils, Geocoder) {
+  	arcgisUtils.createMap("92309d85b34342de8514caefa3df56a5", "map").then(function(response) {
+  		//todo: add stuff
+  		new Geocoder({
+  			map: response.map,
+  			autoNavigate: true,
+  			maxLocations: 20,
+  			value: "Portland, OR coffee shops"
+  		}, "search");
+  	});
   });
