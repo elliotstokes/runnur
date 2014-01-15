c0635a1b9aada77f046bce8553493abacb0cbe06
Elliot Stokes <elliot.stokes@gmail.com>
2014-01-15 22:13:26 +0000

    updates

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	config = require("./application-config.json");
+
+//Set views directory
+app.set('views', __dirname + '/views');
+
 
 /**
 * Main application Route.
 */
 app.get("/", function(req,res) {
-	res.send("runnur");
-	res.end();
+	res.render("index.jade");
 });
+
+/**
+* Get app config
+*/
+app.get("/config", function(req,res) {
+	res.json(config.client);
+});
+
+/**
+* get a token to use routing
+*/
+app.get("/gettoken", function(req, res) {
+	//take oauth app token and secret and exchange for a token
+});
+
+//setup static route
+app.use('/assets', express.static(__dirname + '/assets'));
 
 app.listen(1337);
---
+++ assets/js/runnur.js
@@ -0,0 +1,8 @@
+  require([
+      "esri/arcgis/utils",
+      "dojo/domReady!"
+      ], function(arcgisUtils){
+      arcgisUtils.createMap("92309d85b34342de8514caefa3df56a5", "map").then(function (response) {
+      	//todo: add stuff 
+      });
+  });
